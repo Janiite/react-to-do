b7431eeb69b182221de36d36fb2425c6d1643b0b
MadaraGS <madaragelsberga@inbox.lv>
2022-04-20 13:58:32 +0300

    "add description"

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { FcTodoList } from "react-icons/fc";
+import { BsFillBootstrapFill} from "react-icons/bs";
+import { SiMysql } from "react-icons/si";
+import "../Components/Navbar/Navbar.css";
 
 const About = () => {
   return (
@@ -8,36 +12,42 @@ const About = () => {
         <div className="about-component-box">
         
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3><strong>React Framework</strong></h3>
           </div>
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3>Made by components </h3>
+            <ul><li>Navbar</li>
+              <li>AddTask</li>
+              <li>Button</li> 
+              <li>EditTask</li>
+              <li>Header</li>
+              <li>Tasks</li>
+              <li>Task</li>
+            </ul>
           </div>
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3> <SiMysql size="70px"/></h3>
+            <p>Connected with Axious library and made CRUD</p>
           </div>
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3>useState hooks</h3>
+            <p>Hooks ensure that componets itself changes</p>
           </div>
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3><FcTodoList size="50px" /> Icons & Font Awesome</h3>
+            <p>Icons from react-icon library</p>
           </div>
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3>Router, Switch</h3>
+            <p>Ensure switching between pages as Home, About</p>
           </div>
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3>CSS library </h3>
+            <p><BsFillBootstrapFill/> Bootstrap</p>
           </div>
           <div className="about-component">
-            <h3>Title1</h3>
-            <p>Content, content, content, content, content</p>
+            <h3>The Map() Funtion</h3>
+            <p>To rendering a Tasks to browser</p>
           </div>
           
         </div>
