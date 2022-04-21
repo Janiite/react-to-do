6299cdd366cf39eb24bdb004b7cf0338cf2e98b2
Jana Fedotovska <69121010+Janiite@users.noreply.github.com>
2022-04-21 11:55:40 +0300

    commit

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import MenuItems from './MenuItems'
-import  './Navbar.css'
-import { FcTodoList} from "react-icons/fc"
+import React from "react";
+import MenuItems from "./MenuItems";
+import "./Navbar.css";
+import { FcTodoList } from "react-icons/fc";
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
 
-const [clicked, setClicked] = useState(false);
-  
-const handleClick= () => {
-     {setClicked(!clicked)}
- }
-
+  const handleClick = () => {
+    {
+      setClicked(!clicked);
+    }
+  };
 
   return (
     <nav className="NavbarItems">
-        <h1 className="navbar-logo"><FcTodoList size="50px" /></h1>
-        
-        <ul className={clicked? 'nav-menu active' : 'nav-menu'}>
-        <li><Link to='about'>About</Link></li>
-        <li><Link to='/'>Home</Link></li>
-           
-        </ul>
+    
+        <FcTodoList className="navbar-logo" size="50px" />
+      
 
-
+      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link className="nav-links" to="about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-links" to="/">
+            Home
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
